80b4fa0f5997cd1331770e8d68ba324fdb448f4f
Kidlovec <kidlovec@gmail.com>
2020-03-27 00:19:13 +0800

    init

---
+++ src/study/kidlovec/demo/Main.java
@@ -0,0 +1,13 @@
+package study.kidlovec.demo;
+
+import java.util.HashMap;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        HashMap<String,String> map = new HashMap<>();
+
+    }
+}
